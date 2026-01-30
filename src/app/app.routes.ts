66f4b6a97edf414a44363568b700c3ccfb2768c9
Velki0 <b.gerardin125@gmail.com>
2026-01-30 09:38:35 +0100

    Suppression TP parasite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,11 @@ import { Home } from './site/home/home';
 import { Films } from './site/films/films';
 import { About } from './site/about/about';
 import { Errors } from './site/errors/errors';
-import { ProductGet } from './site/product/product-get/product-get';
-import { ProductAdd } from './site/product/product-add/product-add';
-import { ProductEdit } from './site/product/product-edit/product-edit';
 
 export const routes: Routes = [
     { path:'', component: Home },
     { path: 'films', component: Films },
     { path: 'about', component: About },
-    { path: 'products', component: ProductGet },
-    { path: 'products/add', component: ProductAdd },
-    { path: 'products/edit/:id', component: ProductEdit },
     { path:'404', component: Errors},
     { path:'**', redirectTo: '/404' }
 ];
--- src/app/model/product.ts
+++
@@ -1,6 +0,0 @@
-export interface Product {
-    id: number;
-    ProductName: string;
-    ProductDescription: string;
-    ProductPrice: number;
-}
