382d3fa2a909f9a091c482e1740dc7baa39f5759
Velki0 <b.gerardin125@gmail.com>
2026-01-30 09:25:44 +0100

    Ajout page d'erreurs

--- src/app/site/errors/errors.ts
+++ src/app/site/errors/errors.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-errors',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './errors.html',
   styleUrl: './errors.css',
 })
